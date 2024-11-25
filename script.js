02eec3d8f1b44aa50898adee685fcfd6a7e99aac
gaksss <clement.carlier2@gmail.com>
2024-11-25 15:01:57 +0100

    drumpad function when keyup and sound once

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const inputs = document.querySelector("body");
-
-inputs.addEventListener("keydown", handlePlaySound);
-
+document.addEventListener("keydown", handlePlaySound);
+document.addEventListener("keyup", handleKeyUp);
 function handlePlaySound(event) {
-  let key = document.querySelector('.key[data-key="' + event.keyCode + '"]');
-  let audio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
-
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  if (event.repeat) {
+    return;
+  }
+  if (!key) {
+    return;
+  }
+  if (event.keyCode === 82 || event.keyCode === 80) {
+    console.log(event.keyCode);
+    return;
+  }
   audio.currentTime = 0;
+
   audio.play();
+
   key.classList.add("playing", "sound");
-  setTimeout(() => {
-    key.classList.remove("playing", "sound");
-  }, 300);
+}
+
+function handleKeyUp(event) {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  key.classList.remove("playing", "sound");
 }
