fc69d01ef9723105999bc388e5f691dd85291865
gaksss <clement.carlier2@gmail.com>
2024-11-25 16:56:15 +0100

    function rec finie

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("keydown", handlePlaySound);
 document.addEventListener("keyup", handleKeyUp);
+
 function handlePlaySound(event) {
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
@@ -9,18 +10,37 @@ function handlePlaySound(event) {
   if (!key) {
     return;
   }
-  if (event.keyCode === 82 || event.keyCode === 80) {
-    console.log(event.keyCode);
-    return;
-  }
   audio.currentTime = 0;
-
   audio.play();
-
   key.classList.add("playing", "sound");
 }
 
 function handleKeyUp(event) {
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (!key) {
+    return;
+  }
   key.classList.remove("playing", "sound");
+}
+
+document.addEventListener("keydown", handleRec);
+let recInput = [];
+let rec = false;
+function handleRec(event) {
+  if (event.keyCode === 82 && rec == false) {
+    
+    console.log("début enregistrement");
+    rec = true;
+    return;
+  }
+  if (rec == true) {
+    recInput.push(event.keyCode);
+  }
+  if (event.keyCode === 82 && rec == true) {
+    rec = false;
+    recInput.pop();
+    console.log("enregistrement terminé");
+    console.log(recInput);
+  }
+  
 }
